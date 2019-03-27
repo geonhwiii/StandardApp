9dcda296ca11ea8575be179fd9c1bfcceb384f55
geonhwiii <g01063962671@gmail.com>
2019-03-27 12:08:36 +0900

    Initial Commit

---
+++ StandardApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StandardApp
+//
+//  Created by 정건휘 on 27/03/2019.
+//  Copyright © 2019 gunw. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+   override func viewDidLoad() {
+      super.viewDidLoad()
+      // Do any additional setup after loading the view, typically from a nib.
+   }
+
+
+}
+
